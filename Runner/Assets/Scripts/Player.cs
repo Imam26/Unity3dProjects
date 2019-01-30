680388594fdfcc5b39d4a607867612c743cc4cee
Imam26 <i.m.a.m.sabitov@mail.ru>
2019-01-30 17:26:46 +0600

    add game objects

--- Runner/Assets/Scripts/Player.cs
+++ Runner/Assets/Scripts/Player.cs
@@ -6,14 +6,19 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private Rigidbody rb;    
+    private Rigidbody rb;  
+    
+    [SerializeField]
+    private Rigidbody rbGround;    
     [SerializeField]
     private float speed;
     [SerializeField]
     private float sideSpeed;
+
     public Action onGameOver;
     [SerializeField]
     private float score = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +34,13 @@ public class Player : MonoBehaviour
         }          
         else if(Input.GetKey("d")){
             rb.AddForce(sideSpeed*Time.deltaTime,0,speed*Time.deltaTime);
-        }            
+        }   
+         
+        if(rb.transform.position.z >= rbGround.transform.localScale.z/2 + rbGround.transform.position.z){             
+            rb.transform.position = new Vector3(rbGround.transform.position.x,
+                                                rbGround.transform.position.y+1,
+                                                rbGround.transform.position.z - rbGround.transform.localScale.z/2); 
+        }
     }
 
     private void OnCollisionEnter(Collision other) {
@@ -40,11 +51,17 @@ public class Player : MonoBehaviour
                 onGameOver();
             }
         }
+        else if(other.gameObject.tag == "JumpObstacle"){
+            //rb.MovePosition(rb.transform.position + new Vector3(0,2,0));
+        }
     }
     private void OnTriggerEnter(Collider other) {
         if(other.tag == "Coin"){
             score++;
             Debug.Log("Score: "+score);
+            if(score%5==0){
+                speed+=5;
+            }
             Destroy(other.gameObject);
         }
     }
